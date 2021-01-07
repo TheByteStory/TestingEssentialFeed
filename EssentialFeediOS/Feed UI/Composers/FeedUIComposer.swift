bbbcd9c414cc3ae35853e736c6740264c50409b7
Sharanya Abishek <thebytestory@gmail.com>
2021-01-07 17:12:37 +0530

    Move 'MainQueueDispatchDecorator' to a separate file

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -31,38 +31,7 @@ public final class FeedUIComposer {
 
 }
 
-//Decorator pattern - adds behavior to an instance
 
-private final class MainQueueDispatchDecorator<T> {
-private let decoratee: T
-
-  init(decoratee: T) {
-       self.decoratee = decoratee
-   }
-    func dispatch(completion: @escaping () -> Void) {
-           guard Thread.isMainThread else {
-               return DispatchQueue.main.async(execute: completion)
-           }
-
-           completion()
-       }
-    }
-
-extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-       decoratee.load { [weak self] result in
-       self?.dispatch { completion(result) }
-       }
-   }
-}
-
-extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-   func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-       return decoratee.loadImageData(from: url) { [weak self] result in
-           self?.dispatch { completion(result) }
-       }
-   }
-}
 
 
 private extension FeedViewController {
